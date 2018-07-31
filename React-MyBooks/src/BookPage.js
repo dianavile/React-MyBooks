acb770c4cdc66efde970abfb6a877819314514ed
@dianavile <diana.vile@gmail.com>
2018-07-31 15:40:46 +0200

    Update BookPage.js

--- React-MyBooks/src/BookPage.js
+++ React-MyBooks/src/BookPage.js
@@ -14,23 +14,23 @@ export default class BookPage extends React.Component {
           <div className="book-cover" style={style}/>
           <div className="book-shelf-changer">
 		    {option === "eventForListIndexPage" ? (
-			  <select value={book.shelf} onChange={(event) => handleChange(book, event.target.value)}>
-			    <option value="none" disabled>Move to...</option>
-				<option value="currentlyReading">Currently Reading</option>
-				<option value="wantToRead">Want to Read</option>
-				<option value="read">Read</option>
-				<option value="none">None</option>
-			  </select>
-			  ) : (
-			    <select value={book.shelf} onChange={(event) => updateListBooks(book, event.target.value)}>
-			      <option value="none" disabled>Move to...</option>
-				  <option value="currentlyReading">Currently Reading</option>
-				  <option value="wantToRead">Want to Read</option>
-				  <option value="read">Read</option>
-				  <option value="none">None</option>
-			    </select>
-			  )
-			}
+              <select value={book.shelf} onChange={(event) => handleChange(book, event.target.value)}>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+              ) : (
+                <select value={book.shelf} onChange={(event) => updateListBooks(book, event.target.value)}>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              )
+            }
           </div>
         </div>
         <div className="book-title">{book.title}</div>
