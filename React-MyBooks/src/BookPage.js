499585298e206b9ed2eaa4f8fc9fe9c4f9621a26
@dianavile <diana.vile@gmail.com>
2018-07-31 20:09:36 +0100

    Update BookPage.js

--- React-MyBooks/src/BookPage.js
+++ React-MyBooks/src/BookPage.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 export default class BookPage extends React.Component {
+    existBooksOnQueryResult(bookFromQuery) {
+		const { books } = this.props;
+
+		for (let book of books) {
+			if (book.id === bookFromQuery.id) {
+				return book.shelf;
+			}
+		}
+		
+		return "none";
+	}
+	
     render() {
         const { book, handleChange, option, updateListBooks } = this.props;
         const style = {
@@ -8,6 +20,7 @@ export default class BookPage extends React.Component {
             height: 193,
             backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ""})`
         }
+	
     return (
       <div className="book">
         <div className="book-top">
@@ -22,7 +35,7 @@ export default class BookPage extends React.Component {
                 <option value="none">None</option>
               </select>
               ) : (
-                <select value={book.shelf} onChange={(event) => updateListBooks(book, event.target.value)}>
+                <select value={this.existBooksOnQueryResult(book)} onChange={(event) => updateListBooks(book, event.target.value)}>
                   <option value="none" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
