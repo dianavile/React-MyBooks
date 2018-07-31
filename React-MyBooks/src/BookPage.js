69e466610fa994bed077766465ac0618f4e7b83f
@dianavile <diana.vile@gmail.com>
2018-07-31 09:46:46 +0200

    Update changes to BookPage.js

---
+++ React-MyBooks/src/BookPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default class BookPage extends React.Component {
+    render() {
+        const { book, handleChange } = this.props;
+        const style = {
+            width: 128,
+            height: 193,
+            backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ""})`
+        }
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={style}/>
+          <div className="book-shelf-changer">
+          <select value={book.shelf} onChange={(event)=>handleChange(book, event.target.value)}>
+             <option value="none" disabled>Move to...</option>
+             <option value="currentlyReading">Currently Reading</option>
+             <option value="wantToRead">Want to Read</option>
+             <option value="read">Read</option>
+             <option value="none">None</option>
+           </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
+      </div>
+    );
+}
+}
